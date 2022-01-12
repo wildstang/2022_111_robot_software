c28214699358c0abac3dd3ec63e00d7ada9f54af
jwannebo3524 <56098116+jwannebo3524@users.noreply.github.com>
2022-01-12 10:12:53 -0600

    Update Stuffs.py
    Fixed some bugs. Also added option for extra state information for cortex, predicting input by default (to help w/ discrete sequence prediction), and added decay by default when no reward present.

--- src/main/java/org/wildstang/sample/subsystems/AI/Stuffs.py
+++ src/main/java/org/wildstang/sample/subsystems/AI/Stuffs.py
@@ -35,15 +35,16 @@ class Column:
         return self.Vector[self.Inp,self.Inp+self.Oup]
 
 class Cortex:
-    def __init__(self,n = 10, inp = 100, oup = 5,gamma = 0.99, learn = 0.001,ColumnParameters = [500,10,10,0.99]):
+    def __init__(self,n = 10, inp = 100, oup = 5, extra = 10,gamma = 0.99, learn = 0.001,ColumnParameters = [500,10,10,0.99]):
+        self.cp = ColumnParameters 
         self.Learn = learn
-        self.Waets = (2*np.random.random((n+inp+oup,n+oup)))-1
+        self.Waets = (2*np.random.random(((n*cp[1])+inp+oup+extra,(n*cp[1])+oup+inp+extra)))-1 #now square
         self.Inp = inp
         self.Oup = oup
         self.Gamma = gamma
         self.Columns = []
-        self.cp = ColumnParameters
         self.Vector = np.zeros((n+oup))#?????? no??? 
+        self.Extra = extra
         c = 0
         while(c<n):
             self.Columns.append(Column(self.cp[0],self.cp[1],self.cp[2],self.cp[3]))
@@ -62,19 +63,20 @@ class Cortex:
             c += 1
     def Sigmoid(self,v):
         return np.nan_to_num((np.exp(v)/(np.exp(v)+1)))
-    def Update(self,inp,oup = None,Reward = 1):
+    def Update(self,inp = None,oup = None,Reward = 1):
         self.Last = self.Vector
-        totalIn = np.concatenate((inp,self.Vector))
-        self.Vector = self.Sigmoid(np.dot(self.Waets,totalIn))
+        if (not inp == None):
+            self.Vector[len(self.Columns)*self.cp[1]:len(self.Columns)*self.cp[1]+self.Inp] = inp
+        self.Vector = self.Sigmoid(np.dot(self.Waets,self.Vector))
         c = 0
         while(c<len(self.Columns)):
             blurgh = self.Columns[c].Update(inp = self.Vector[c*self.cp[1]:(c+1)*self.cp[1]],Learn = Reward*self.Learn)
             self.Vector[c*self.cp[1]:(c+1)*self.cp[1]] = self.Columns[c].Read()
             c += 1
-        if(oup):
-            self.Vector[c*self.cp[1]:] = oup
-        if (not (self.Learn*Reward == 0)):
+        if(not oup == None):
+            self.Vector[c*self.cp[1]+self.Inp+self.Extra:] = oup
+        if (not (self.Learn == 0)):
             self.Waets = (self.Waets*self.Gamma) +(self.Learn*np.outer(self.Vector,self.Last))
         return "SUPERRBLUURRGGH."
     def GetOutput(self):
-        return self.Vector[len(self.Columns)*self.cp[1]:]
+        return self.Vector[len(self.Columns)*self.cp[1]+self.Inp+self.Extra:]
