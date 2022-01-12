78038fa2adbab44cbbf2a25540e9347a088504d7
jwannebo3524 <56098116+jwannebo3524@users.noreply.github.com>
2022-01-12 13:08:35 -0600

    Update Stuffs.py
    Add epsilon value and return values for diagnostics

--- src/main/java/org/wildstang/sample/subsystems/AI/Stuffs.py
+++ src/main/java/org/wildstang/sample/subsystems/AI/Stuffs.py
@@ -18,10 +18,10 @@ class Column:
         self.Waets = np.load(file+"W.npy")
         self.Vector = np.load(file+"V.npy")
     
-    def Sigmoid(self,v):
+    def Sigmoid(self,v): #sigmoid function
         return np.nan_to_num((np.exp(v)/(np.exp(v)+1)))
 
-    def Update(self,inp = None,oup = None,Learn = 0):
+    def Update(self,inp = None,oup = None,Learn = 0): #do stuff
         self.Last = self.Vector
         self.Vector = self.Sigmoid(np.dot(self.Waets,self.Vector)) #no biases? W matrix format is (neurons,connections)
         if (inp):
@@ -31,11 +31,11 @@ class Column:
         if (not (Learn == 0)):
             self.Waets = (self.Waets*self.Gamma) +(Learn*np.outer(self.Vector,self.Last))
         return "BLURGH"
-    def Read(self):
+    def Read(self): #get output
         return self.Vector[self.Inp,self.Inp+self.Oup]
 
 class Cortex:
-    def __init__(self,n = 10, inp = 100, oup = 5, extra = 10,gamma = 0.99, learn = 0.001,ColumnParameters = [500,10,10,0.99]):
+    def __init__(self,n = 10, inp = 100, oup = 5, extra = 10,gamma = 0.99, learn = 0.001, epsilon = 0.01, ColumnParameters = [500,10,10,0.99]):
         self.cp = ColumnParameters 
         self.Learn = learn
         self.Waets = (2*np.random.random(((n*cp[1])+inp+oup+extra,(n*cp[1])+oup+inp+extra)))-1 #now square
@@ -45,12 +45,15 @@ class Cortex:
         self.Columns = []
         self.Vector = np.zeros((n+oup))#?????? no??? 
         self.Extra = extra
+        self.Epsilon = epsilon
+        #self.ListOfVectors = [] could use too much memory, so implementing in TimeSeriesPredictor instead.
         c = 0
         while(c<n):
             self.Columns.append(Column(self.cp[0],self.cp[1],self.cp[2],self.cp[3]))
             c += 1
         print("BLUUURRRGGGH!?")
-
+    
+    #save/load
     def Save(self,file):
         c = 0
         while(c<len(self.Columns)):
@@ -61,6 +64,9 @@ class Cortex:
         while(c<len(self.Columns)):
             self.Columns[c].Load(file+"C"+str(c))
             c += 1
+      
+    
+    #main functions
     def Sigmoid(self,v):
         return np.nan_to_num((np.exp(v)/(np.exp(v)+1)))
     def Update(self,inp = None,oup = None,Reward = 1):
@@ -73,6 +79,9 @@ class Cortex:
             blurgh = self.Columns[c].Update(inp = self.Vector[c*self.cp[1]:(c+1)*self.cp[1]],Learn = Reward*self.Learn)
             self.Vector[c*self.cp[1]:(c+1)*self.cp[1]] = self.Columns[c].Read()
             c += 1
+        
+        if(self.Epsilon >0):
+            self.Vector += ((2*np.random.random(len(self.Vector)))-1)*self.Epsilon
         if(not oup == None):
             self.Vector[c*self.cp[1]+self.Inp+self.Extra:] = oup
         if (not (self.Learn == 0)):
@@ -80,3 +89,16 @@ class Cortex:
         return "SUPERRBLUURRGGH."
     def GetOutput(self):
         return self.Vector[len(self.Columns)*self.cp[1]+self.Inp+self.Extra:]
+   
+
+    #for diagnostics
+    def GetColumnActivities(self):
+        Avgs = []
+        c = 0
+        while(c<len(self.Columns)):
+            Avgs.append(np.sum(np.abs(self.Columns[c].Vector))/len(self.Columns[c].Vector)
+            c += 1
+        return Avgs
+    def GetNeuron(self,c,n):
+        return self.Columns[c].Vector[n]
+                        
