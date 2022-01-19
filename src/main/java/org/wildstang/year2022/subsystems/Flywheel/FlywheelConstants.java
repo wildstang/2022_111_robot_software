dad5001b067398c98e5a19da60f2d9649662db11
jwannebo3524 <56098116+jwannebo3524@users.noreply.github.com>
2022-01-19 17:42:52 -0600

    yay

---
+++ src/main/java/org/wildstang/year2022/subsystems/Flywheel/FlywheelConstants.java
@@ -0,0 +1,12 @@
+package org.wildstang.year2022.subsystems.Flywheel;
+
+public class FlywheelConstants{
+
+    //placeholder robot length
+    public static final double robotLength = 11.5;
+    //placeholder robot width
+    public static final double robotWidth = 11.5;
+    //placeholder angle for the flywheel's 
+
+    
+}
---
+++ src/main/java/org/wildstang/year2022/subsystems/FlywheelTurret/FlywheelConstants.java
@@ -0,0 +1,16 @@
+package org.wildstang.year2022.subsystems.FlywheelTurret;
+
+public class FlywheelConstants{
+
+    //placeholder robot length
+    public static final double robotLength = 11.5;
+    //placeholder robot width
+    public static final double robotWidth = 11.5;
+    //placeholder robot length
+    public static final double HOOD_MOTOR = 90;
+    //right motor that moves in positive direction
+    public static final double RIGHT_MOTOR = 52.8;
+    //left motor that moves in negative direction
+    public static final double LEFT_MOTOR = -52.8;
+    
+}
