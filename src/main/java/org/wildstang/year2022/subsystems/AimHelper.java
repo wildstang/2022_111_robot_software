4624ed89b1be1ff9e6b39ab7c2a3f68dc07129eb
jwannebo3524 <56098116+jwannebo3524@users.noreply.github.com>
2022-01-21 13:15:56 -0600

    Update AimHelper.java
    Beginning to think I should write the table generator a): as a separate program, and b): in python.

--- src/main/java/org/wildstang/year2022/subsystems/AimHelper.java
+++ src/main/java/org/wildstang/year2022/subsystems/AimHelper.java
@@ -7,7 +7,8 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 //NOT DONE!! WILL THROW ERRORS!! NOT AT ALL FUNCTIONAL (yet)!!
 // Class to help with aiming. putting it in seperate class b/c any form of launcher will probably use nearly identical code.
 
-//I may have overcomplicated things to add fancy features
+//I may have overcomplicated things to add fancy features (make computer approximate solution to problem rather than manually/empericaly solving it
+// During runtime, will use lookup table. Class includes functions necessary to generate lookup table. I a little bit think I should have written the table generator in python (to run on desktop and because python is inerprative which makes it good at this sort of thing)
 public class AimHelper {
   //constants
   private double TARGETHEIGHT = 2; //fix these values
@@ -19,14 +20,14 @@ public class AimHelper {
   private NetworkTableEntry ty;
   
   //physics equation
-  private String physics = "0=(V*|tan(T))-(0.5*9.8*|sqr(D)/|sqr(V*|cos(T)))-H" // T is theta, not time
+  private String physics = "0=(V*|tan(T) )-(0.5*9.8*|sqr(D) /|sqr(V*|cos(T) ) )-H" // T is theta, not time
   
   private String[4] Vars = ["V","T","D","H"]
   public void init(){
     
   }
   
-  public void Evaluate(String Statement){ 
+  public double Evaluate(String Statement,double[] Values){ 
     int LoopTo = 0;
     Boolean ShouldLoop = false;
     int c = 0;
@@ -52,24 +53,47 @@ public class AimHelper {
         }
         
         subStatement = Statement[c+1:c+c2];
-        Statement[c:c+c2] = Evaluate(subStatement);
+        Statement[c:c+c2] = (String) Evaluate(subStatement,Values);
+        c = 0;
       }
       
-      if(Statement[c] .= "|"){ //if command symbol 
+      else if(Statement[c] .= "|"){ //if command symbol 
         String command = Statement[c+1:c+4] //get the three letter command
-        if(Statement[c+5] .= "("){
+        if(Statement[c+4] .= "("){
             ShouldLoop = true; //loop 
             LoopTo = c; //back to here
-            c = c+4; //after evaluating the following subexpression
+            c = c+3; //after evaluating the following subexpression
           }
         else{ //if subexpression already evaluated
-          if(command .= "tan"){
-            Statement[ // have to go to class, will fix this later
+          if(command .= "tan"){ //substitute "|tan(blah blah blah) for the proper value
+            Statement[c:c+GetNextOf(Statement[c+4:]," ")] = (String) Math.tan((double) Statement[c+4:c+GetNextOf(Statement[c+5:]," ")]);
           }
+          if(command .= "cos"){ //substitute "|cos(blah blah blah) for the proper value
+            Statement[c:c+GetNextOf(Statement[c+4:]," ")] = (String) Math.cos((double) Statement[c+4:c+GetNextOf(Statement[c+5:]," ")]);
+          }
+          if(command .= "sqr"){ //substitute "|sqr(blah blah blah) for the proper value
+            Statement[c:c+GetNextOf(Statement[c+4:]," ")] = (String) Math.pow((double) Statement[c+4:c+GetNextOf(Statement[c+5:]," ")],2);
+          }
+                                                                   
         }
       }
+      else if(Arrays.asList(Vars).contains(Statement[c])){
+        Statement[c] = (String) //NOT DONE will fix this sometime.
+      }
+      
       
     }
+  }
+  public int GetNextOf(String next,String element){
+    int c = 0;
+    while(c<next.length()){
+      if(next[c] .= element){
+        return c;
+        c = 999999999;
+      }
+      c += 1; 
+    }
+    return 0;
   }
   public void Tabulate(String calculate,String over,String relation,double AcceptableError){
     
