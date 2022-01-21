4ed6c61797a4ef52bcf887022b8ece3499f45872
jwannebo3524 <56098116+jwannebo3524@users.noreply.github.com>
2022-01-21 10:10:03 -0600

    Update CatapaultMain.java
    Add comments

--- src/main/java/org/wildstang/year2022/subsystems/Catapault/CatapaultMain.java
+++ src/main/java/org/wildstang/year2022/subsystems/Catapault/CatapaultMain.java
@@ -1,5 +1,5 @@
-package org.wildstang.year2022.subsystems.Catapault;
-
+package org.wildstang.year2022.subsystems.Catapault; 
+// ton of imports
 import org.wildstang.framework.subsystems.Subsystem;
 
 import com.ctre.phoenix.motion.MotionProfileStatus;
@@ -39,13 +39,13 @@ public class CatapaultMain {
     private WsSparkMax Latch, Windup, Angle; // motors
 
 
-    private WsAnalogInput ManualTargetDistance; 
+    private WsAnalogInput ManualTargetDistance; //joystick or trigger for maual aiming by distance
 
-    private WsDigitalInput Launch; 
+    private WsDigitalInput Launch; //launch button
 
-    private WsDigitalInput ManualWindup; // if there is windup problem
+    private WsDigitalInput ManualWindup; // button to click if there is windup problem
 
-    private WsDigitalInput AutoTarget;
+    private WsDigitalInput AutoTarget; //hold to auto-aim
 
 
 
@@ -54,39 +54,40 @@ public class CatapaultMain {
     private double TargetDist;
     private double AimingAngle;
 
-    private Boolean IsLoaded;
+    private Boolean IsLoaded; //to keep track of current state. could use enum
     private Boolean IsFiring;
 
 
     private double MAXDIST = 100; // in meters??
 
-    private double DEADBAND = 0.05;
+    private double DEADBAND = 0.05; //for the manual targeting.
     
     private double LATCHED = 0; //latched position in encoder ticks
     
-    private double UNLATCHED = 1024; 
+    private double UNLATCHED = 1024; //unlatched position in encoder ticks
     
     private double WIND_SPEED = 0.3; // percent power to run windup motor at
     
     private double WOUND = 1000; // wound position in non-modal encoder ticks. 
     private double UNWOUND = 0;
     
-    private double windupPosition; 
+    private double windupPosition; // to keep track of windup motor's position
 
     
 
     // For Limelight:
-    private NetworkTable LimeTable;
-    private NetworkTableEntry ty;
-    private NetworkTableEntry tx;
-    private NetworkTableEntry thor; // t horizontal
-    private NetworkTableEntry tvert;
+    private NetworkTable LimeTable; //Stolen from 2020 code
+    private NetworkTableEntry ty; //y position of target in camera veiw
+    private NetworkTableEntry tx; //x position
+    private NetworkTableEntry thor; // t horizontal (i think its the target width. didn't end up using it)
+    //also the god of Thursdays 
+    private NetworkTableEntry tvert; // t vertical
     
     
-    private double CameraViewAngle = Math.PI/4; //fixthis
-    private double CAMMAXY = 100; //fixthis
-    private double TARGHEIGHTABOVECAMERA = 10; //fixthis
-    private double CAMABOVECAT = 0.25; //fix
+    private double CameraViewAngle = Math.PI/4; //fixthis. The fov of the camera in radians.
+    private double CAMMAXY = 100; //fixthis.  Number of vertical divisions in limelight y output
+    private double TARGHEIGHTABOVECAMERA = 10; //fixthis. Height between camera and targer
+    private double CAMABOVECAT = 0.25; //fix. Catapault is actually above camera, so name is backwards.
 
     private double TICKS = 4096; //ticks per rotation for catapault
 
@@ -94,11 +95,11 @@ public class CatapaultMain {
 
 
       Latch = (WsSparkMax) Core.getOutputManager().getOutput(WSOutputs.CAT_LATCH); //vars not avalable until we decide
-      Windup = (WsSparkMax) Core.getOutputManager().getOutput(WSOutputs.CAT_WIND);
+      Windup = (WsSparkMax) Core.getOutputManager().getOutput(WSOutputs.CAT_WIND); //init sparkmaxes
       Angle = (WsSparkMax) Core.getOutputManager().getOutput(WSOutputs.CAT_ANGLE);
       
-
-      ManualTargetDistance = (WsAnalogInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_LEFT_JOYSTICK_Y);
+       //init inputs
+      ManualTargetDistance = (WsAnalogInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_LEFT_JOYSTICK_Y); 
       ManualTargetDistance.addInputListener(this);
       
       Launch = (WsDigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_FACE_DOWN);
@@ -107,11 +108,11 @@ public class CatapaultMain {
       ManualWindup = (WsDigitalInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_FACE_UP);
       ManualWindup.addInputListener(this);
       
-      //swerve
+      //swerve. so that auto-targeting can override drive base commands for horizontal aiming. Currently not implemented.
       driveBase = new SwerveDrive();
 
 
-      //limelight
+      //limelight. Code stolen from 2020
       LimeTable  = NetworkTableInstance.getDefault().getTable("limelight-stang");
 
       ty = LimeTable.getEntry("ty");
@@ -144,35 +145,37 @@ public class CatapaultMain {
       // double inputValue = inputName.getValue();
         
       // handle input
-      if(Launch.getValue() && IsLoaded){
+      if(Launch.getValue() && IsLoaded){ //if you click launch, and not catapault is loaded
         IsLoaded = false;
         IsFiring = true;
       }
-      else if (ManualWindup.getValue()){
+      else if (ManualWindup.getValue()){ //if you click the manual windup button, go to reloading state (which is neither loaded nor firing)
         IsLoaded = false;
         IsFiring = false;
       }
-      if (Math.abs(ManualTargetDistance.getValue())>= DEADBAND){
+      if (Math.abs(ManualTargetDistance.getValue())>= DEADBAND){ //for manual distance input
         TargetDist = ManualTargetDistance.getValue()*MAXDIST;
+          //TODO: calculate angle (copy/paste from autotarget section)
       }
       else{
-          if (AutoTarget.getValue()){
-            double TargetAngle = Math.asin(ty.getDouble(0)/CAMMAXY);
-            TargetDist = TARGHEIGHTABOVECAMERA/Math.tan(TargetAngle);
-            AimingAngle = (TICKS/Math.PI)*Math.atan((TARGHEIGHTABOVECAMERA-CAMABOVECAT)/TargetDist);
-             //Set automatic target dist here.
+          if (AutoTarget.getValue()){ // auto-targeting code. I screwed up and forgot the physics bit, so right now it just acts as though the ball travels in a straigt line. Will fix.
+            double TargetAngle = Math.asin(ty.getDouble(0)/CAMMAXY); // angle between camera and target. Currently assumes camera is pointed straight forward, but if there is offset it should be added here.
+            TargetDist = TARGHEIGHTABOVECAMERA/Math.tan(TargetAngle); //distance between robot and target
+            AimingAngle = (TICKS/Math.PI)*Math.atan((TARGHEIGHTABOVECAMERA-CAMABOVECAT)/TargetDist); //angle between catapault and target
+          //TODO: physics
+              //Set automatic target dist here.
           }          
       }
         
         
       // non- input based state changes
-      if(!IsLoaded &&! IsFiring){
+      if(!IsLoaded &&! IsFiring){  // check if catapault has finished winding and change state accordingly
           if(windupPosition >= WOUND){
               IsLoaded = true;
           }
       }
       if(IsFiring){
-          if(windupPosition <= UNWOUND){ //once catapault unwinds, isfiring is no longer true.
+          if(windupPosition <= UNWOUND){ //once catapault finishes unwinding, isfiring is no longer true.
               windupPosition = UNWOUND;
               IsFiring = false;
           }
@@ -184,23 +187,28 @@ public class CatapaultMain {
   
   
     public void update(){
-        if(!IsLoaded){
+        if(!IsLoaded){ // if not (supposed to be) loaded, unlatch catapault
             Latch.setPosition(UNLATCHED);
         }
-        else{
+        else{ //otherwise, keep it latched
             Latch.setPosition(LATCHED);
         }
-        if(!IsLoaded && !IsFiring){
+        if(!IsLoaded && !IsFiring){ //if not loaded or firing (so is reloading), run the windup motor
             Windup.setSpeed(WIND_SPEED);
         }
-        else{
+        else{ //otherwise don't
             Windup.setSpeed(0);
         }
         
-        Angle.setPosition(AimingAngle);
+        Angle.setPosition(AimingAngle); //set angle control to proper position
+        if(WindupPosition < (TICKS/2)){ //otherwise it probably moved backwards. Value may need tuning if catapault launches to fast.
+            windupPosition += Windup.getPosition(); // keep track of ticks the motor has gone since last encoder reset
+        }
+        else{
+            windupPosition -= Windup.getPosition();
+        }
         
-        windupPosition += Windup.getPosition(); // keep track of ticks the motor has gone.
-        Windup.resetEncoder();
+        Windup.resetEncoder(); //reset encoder, so next Windup.getPosition() will return change in position since this frame
       
     }
 
@@ -210,7 +218,7 @@ public class CatapaultMain {
      * connected components.
      */
     //@Override
-    public void resetState(){
+    public void resetState(){ //assume the catapault starts properly loaded
         IsLoaded = true;
         IsFiring = false;
         AimingAngle = 0;
@@ -228,7 +236,7 @@ public class CatapaultMain {
      */
    // @Override
     public String getName(){
-      return "Cat?";
+      return "Cat?"; 
     }
 
 }
