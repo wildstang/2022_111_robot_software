61cfd17942e099bda2773a1b895a83d11adc4f46
bwaters2460 <bwaters2460@stu.d214.org>
2022-01-26 20:07:48 -0600

    Create Hood.java

---
+++ src/main/java/org/wildstang/year2022/subsystems/Hood/Hood.java
@@ -0,0 +1,61 @@
+package org.wildstang.year2022.subsystems;
+
+import org.wildstang.framework.core.Core;
+import org.wildstang.framework.io.inputs.Input;
+import org.wildstang.framework.subsystems.Subsystem;
+import org.wildstang.hardware.roborio.inputs.WsJoystickAxis;
+import org.wildstang.year2022.robot.WSInputs;
+import org.wildstang.year2022.robot.WSOutputs;
+
+import org.wildstang.hardware.roborio.outputs.WsSparkMax;
+
+/**
+ * Hood system
+ * Left Joystick Y: controls hood angle
+ * dpad: preset angle
+ * Left Trigger: Auto Aim
+ * @author Bernard
+ */
+public class SampleSubsystem implements Subsystem {
+
+    WsSparkMax hood_motor;
+    WsJoystickAxis js;
+    
+    double hoodposition
+    
+
+    @Override
+    public void init() {
+        hood_motor = (WsSparkMax) Core.getOutputManager().getOutput(WSOutputs.HOOD_MOTOR);
+        left_joystick_y = (WsJoystickAxis) Core.getInputManager().getInput(WSInputs.DRIVER_LEFT_JOYSTICK_Y);
+
+        hoodposition = 0.0;
+    }
+
+    @Override
+    public void update() {
+        hood_motor.setPosition(hood_position);
+    }
+
+    @Override
+    public void inputUpdate(Input source) {
+     if (left_joystick_y.getValue() > 0){
+         hood_position = (left_joystick_y.getValue());
+     }
+    }
+
+    @Override
+    public void selfTest() {
+
+    }
+
+    @Override
+    public void resetState() {
+
+    }
+
+    @Override
+    public String getName() {
+        return "Sample";
+    }
+}
