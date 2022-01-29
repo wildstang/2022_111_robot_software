e69a1791edcfcca3cad6444cbc07415a57d95b3f
StabStabImAChicken <77760254+StabStabImAChicken@users.noreply.github.com>
2022-01-29 11:10:52 -0600

    Created Launcher.java
    I added Launcher.java and set up motors related to it in WsOutputs. I also set up the motors, solenoid, and trigger related to the program and tried to set up the Update function a little bit

--- src/main/java/org/wildstang/year2022/robot/WSOutputs.java
+++ src/main/java/org/wildstang/year2022/robot/WSOutputs.java
@@ -43,6 +43,10 @@ public enum WSOutputs implements Outputs {
     ANGLE3("Module 3 Angle Motor", new WsSparkMaxConfig(CANConstants.ANGLE3, true)),
     DRIVE4("Module 4 Drive Motor", new WsSparkMaxConfig(CANConstants.DRIVE4, true)),
     ANGLE4("Module 4 Angle Motor", new WsSparkMaxConfig(CANConstants.ANGLE4, true)),
+
+    LAUNCHER_LEFT(“Left Launcher Motor”, new WsSparkMaxConfig(1, true)),
+    LAUNCHER_RIGHT(“Right Launcher Motor”, new WsSparkMaxConfig(1, true)),
+    KICKER(“Kicker Motor”, new WsSparkMaxConfig(1, true)),
     
 
     // ---------------------------------
@@ -60,6 +64,7 @@ public enum WSOutputs implements Outputs {
     // ********************************
 
     TEST_SOLENOID("Test Solenoid", new WsSolenoidConfig(PneumaticsModuleType.REVPH, 0, false)),
+    LAUNCHER_SOLENOID("Launcher Solenoid", new WsSolenoidConfig(PneumaticsModuleType.REVPH, 0, false)),
     
     // ********************************
     // Relays
---
+++ src/main/java/org/wildstang/year2022/subsystems/launcher/Launcher.java
@@ -0,0 +1,84 @@
+package org.wildstang.year2022.subsystems.launcher;
+
+import org.wildstang.year2022.robot.CANConstants;
+import org.wildstang.year2022.robot.WSInputs;
+import org.wildstang.year2022.robot.WSOutputs;
+import org.wildstang.year2022.robot.WSSubsystems;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import org.wildstang.framework.core.Core;
+import org.wildstang.framework.io.Input;
+import org.wildstang.framework.io.inputs.AnalogInput;
+import org.wildstang.framework.io.inputs.DigitalInput;
+import org.wildstang.framework.pid.PIDConstants;
+import org.wildstang.framework.subsystems.Subsystem;
+import org.wildstang.framework.timer.WsTimer;
+import org.wildstang.hardware.roborio.inputs.WsAnalogInput;
+import org.wildstang.hardware.roborio.outputs.WsSolenoid;
+import org.wildstang.hardware.roborio.outputs.WsSparkMax;
+import org.wildstang.hardware.roborio.outputs.config.WsServoConfig;
+
+public final class Launcher{
+
+    private AnalogInput FIRE_TRIGGER;
+
+    private WsSparkMax leftFlywheelMotor, rightFlywheelMotor, kickerMotor;
+
+    private WsSolenoid ballLatch;
+
+    private int speed, kickerSpeed;
+
+    private int reverseVariable = -1;
+    //We don't know which motor is going to be in the negative direction, so we will worry about this later
+
+    @Override
+    public void init() {
+        leftFlywheelMotor = (WsSparkMax) Core.getOutputManager().getOutput(WSOutputs.LAUNCHER_LEFT);
+        rightFlywheelMotor = (WsSparkMax) Core.getOutputManager().getOutput(WSOutputs.LAUNCHER_RIGHT);
+        kickerMotor = (WsSparkMax) Core.getOutputManager().getOutput(WSOutputs.KICKER);
+
+        ballLatch = (WsSolenoid) Core.getOutputManager().getOutput(WSOutputs.LAUNCHER_SOLENOID);
+
+        FIRE_TRIGGER = (WsAnalogInput) Core.getInputManager().getInput(WSInputs.MANIPULATOR_RIGHT_TRIGGER);
+    }
+
+    @Override
+    public void update() {
+        leftFlywheelMotor.setValue(FIRE_TRIGGER.getValue()/* * reverseVariable*/);
+        rightFlywheelMotor.setValue(FIRE_TRIGGER.getValue()/* * reverseVariable*/);
+        //we will uncomment whichever we are using backwards later
+    }
+
+    @Override
+    public void inputUpdate(Input source) {
+
+    }
+
+    @Override
+    public void selfTest() {
+       
+    }
+
+    @Override
+    public void resetState() {
+        speed = 0;
+    }
+
+    @Override
+    public String getName() {
+        return "Launcher";
+    }
+
+}
+
+//leftFlywheelMotor.setValue(speed);
+//rightFlywheelMotor.setValue(speed);
+    //I will put these somehwere when I figure out what to do with them
