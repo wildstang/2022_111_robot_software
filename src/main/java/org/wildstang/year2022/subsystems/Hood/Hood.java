0cea8b97d4c5824f61bf13bdfb081b12a9bfa6c4
dfruzyna2137 <43018879+dfruzyna2137@users.noreply.github.com>
2022-02-05 10:29:30 -0600

    Added MA3 & camel case
    added MA3 to define hood position in reset state, camel case to all variables, and titles to each object group

--- src/main/java/org/wildstang/year2022/subsystems/Hood/Hood.java
+++ src/main/java/org/wildstang/year2022/subsystems/Hood/Hood.java
@@ -1,6 +1,9 @@
 package org.wildstang.year2022.subsystems.Hood;
 
 
+
+import com.revrobotics.SparkMaxAnalogSensor.Mode;
+
 import org.wildstang.framework.core.Core;
 import org.wildstang.framework.io.inputs.Input;
 import org.wildstang.framework.io.inputs.AnalogInput;
@@ -22,74 +25,79 @@ import org.wildstang.year2022.subsystems.Hood.AimHelper;
  */
 public class Hood implements Subsystem {
 
-    WsSparkMax hood_motor;
+    //Motors
+    WsSparkMax hood_Motor;
 
-    WsJoystickAxis left_joystick_y;
-    AnalogInput left_trigger;
-    DigitalInput dpad_up;
-    DigitalInput dpad_down;
-    DigitalInput dpad_left;
-    DigitalInput dpad_right;
+    //Inputs
+    WsJoystickAxis left_Joystick_y;
+    AnalogInput left_Trigger;
+    DigitalInput dpad_Up;
+    DigitalInput dpad_Down;
+    DigitalInput dpad_Left;
+    DigitalInput dpad_Right;
 
-    double hood_position;
-    double range_constant;
+    //Variables
+    private double hood_Position;
+    private double range_constant;
     
-    final double preset1 = 1;
-    final double preset2 = .75;
-    final double preset3 = .5;
-    final double preset4 = .25;
+    //Constants
+    private final double PRESET1 = 1;
+    private final double PRESET2 = .75;
+    private final double PRESET3 = .5;
+    private final double PRESET4 = .25;
 
-    final double max_angle = 45;
-    final double position_change = .02;
+    private final double MAX_ANGLE = 45;
+    private final double POSITION_CHANGE = .02;
     
     AimHelper aim;
     
     @Override
     public void init() {
-        hood_motor = (WsSparkMax) Core.getOutputManager().getOutput(WSOutputs.HOOD_MOTOR);
-        hood_motor.initClosedLoop(1.0, 0.0, 0.0, 0.0);
-        left_joystick_y = (WsJoystickAxis) Core.getInputManager().getInput(WSInputs.DRIVER_LEFT_JOYSTICK_Y);
-        left_trigger = (AnalogInput) Core.getInputManager().getInput(WSInputs.DRIVER_LEFT_TRIGGER);
-        dpad_up = (DigitalInput) Core.getInputManager().getInput(WSInputs.DRIVER_DPAD_UP);
-        dpad_up.addInputListener(this);
-        dpad_down = (DigitalInput) Core.getInputManager().getInput(WSInputs.DRIVER_DPAD_DOWN);
-        dpad_down.addInputListener(this);
-        dpad_left = (DigitalInput) Core.getInputManager().getInput(WSInputs.DRIVER_DPAD_LEFT);
-        dpad_left.addInputListener(this);
-        dpad_right = (DigitalInput) Core.getInputManager().getInput(WSInputs.DRIVER_DPAD_RIGHT);
-        dpad_right.addInputListener(this);
+        hood_Motor = (WsSparkMax) Core.getOutputManager().getOutput(WSOutputs.HOOD_MOTOR);
+        hood_Motor.initClosedLoop(1.0, 0.0, 0.0, 0.0);
+        left_Joystick_y = (WsJoystickAxis) Core.getInputManager().getInput(WSInputs.DRIVER_LEFT_JOYSTICK_Y);
+        left_Trigger = (AnalogInput) Core.getInputManager().getInput(WSInputs.DRIVER_LEFT_TRIGGER);
+        dpad_Up = (DigitalInput) Core.getInputManager().getInput(WSInputs.DRIVER_DPAD_UP);
+        dpad_Up.addInputListener(this);
+        dpad_Down = (DigitalInput) Core.getInputManager().getInput(WSInputs.DRIVER_DPAD_DOWN);
+        dpad_Down.addInputListener(this);
+        dpad_Left = (DigitalInput) Core.getInputManager().getInput(WSInputs.DRIVER_DPAD_LEFT);
+        dpad_Left.addInputListener(this);
+        dpad_Right = (DigitalInput) Core.getInputManager().getInput(WSInputs.DRIVER_DPAD_RIGHT);
+        dpad_Right.addInputListener(this);
         aim = new AimHelper();
+        
         resetState();
     }
 
     @Override
     public void update() {
-        hood_motor.setPosition(hood_position * range_constant);
+        hood_Motor.setPosition(hood_Position * range_constant);
     }
 
     @Override
     public void inputUpdate(Input source) {
     
-    if (source == dpad_up){
-        hood_position = preset1;
+    if (source == dpad_Up){
+        hood_Position = PRESET1;
     }
-    if (source == dpad_down){
-        hood_position = preset2;
+    if (source == dpad_Down){
+        hood_Position = PRESET2;
     }
-    if (source == dpad_left){
-        hood_position = preset3;
+    if (source == dpad_Left){
+        hood_Position = PRESET3;
     }
-    if (source == dpad_right){
-        hood_position = preset4;
+    if (source == dpad_Right){
+        hood_Position = PRESET4;
     }     
-    if (left_trigger.getValue() > 0.5){
-            hood_position = aim.getAngle() / max_angle;
+    if (left_Trigger.getValue() > 0.5){
+            hood_Position = aim.getAngle() / MAX_ANGLE;
          }
      
-     else if (left_joystick_y.getValue() > .15 && hood_position < 1){
-         hood_position += position_change;
-     }else if (left_joystick_y.getValue() < -.15 && hood_position > 0){
-         hood_position += position_change * -1;
+     else if (left_Joystick_y.getValue() > .15 && hood_Position < 1){
+         hood_Position += POSITION_CHANGE;
+     }else if (left_Joystick_y.getValue() < -.15 && hood_Position > 0){
+         hood_Position += POSITION_CHANGE * -1;
      }
      
     }
@@ -101,8 +109,8 @@ public class Hood implements Subsystem {
 
     @Override
     public void resetState() {
-        hood_position = 0.0;
-        range_constant = max_angle / 360.0;
+        hood_Position = (hood_Motor.getController().getAnalog(Mode.kAbsolute).getVoltage() /3.3);
+        range_constant = MAX_ANGLE / 360.0;
     }
 
     @Override
