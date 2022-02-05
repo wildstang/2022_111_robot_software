165cad34bfd8aa94c0576b5c1c0e042f37cc6e9c
potatomasqer <56097975+potatomasqer@users.noreply.github.com>
2022-02-05 09:08:32 -0600

    added nutural and absolute encoder
    also added..
    .initClosedLoop(1.0,0.0,0.0,0.0)
    resetEncoder();
    a few vars
    .setPosition
    and a large set value command that can be customized when we know the values.

--- src/main/java/org/wildstang/year2022/subsystems/Hood/HoodManager.java
+++ src/main/java/org/wildstang/year2022/subsystems/Hood/HoodManager.java
@@ -3,7 +3,7 @@ import org.wildstang.framework.core.Core;
 import org.wildstang.framework.subsystems.SubsystemManager;
 import org.wildstang.hardware.roborio.inputs.WsAnalogInput;
 import org.wildstang.hardware.roborio.inputs.WsDigitalInput;
-import org.wildstang.hardware.roborio.outputs.WsPhoenix;
+import org.wildstang.hardware.roborio.inputs.WsAbsoluteEncoder;
 import org.wildstang.hardware.roborio.outputs.WsSparkMax;
 import org.wildstang.year2022.robot.WSInputs;
 import org.wildstang.year2022.robot.WSOutputs;
@@ -11,6 +11,7 @@ import org.wildstang.hardware.roborio.inputs.WsJoystickAxis;
 import org.wildstang.framework.subsystems.Subsystem;
 import org.wildstang.framework.io.inputs.Input;
 import java.util.Arrays;
+
 
 //Goals bind controls to the asigned buttons. refrence aim helper 
 
@@ -20,6 +21,8 @@ public class HoodManager implements Subsystem{
     private WsAnalogInput AutoAim;
     private LimeConsts LC;
     private AimHelper Aim;
+
+    private WsAbsoluteEncoder trueEncoder;
     //presets
     private WsDigitalInput Preset1;
     private WsDigitalInput Preset2;
@@ -32,7 +35,9 @@ public class HoodManager implements Subsystem{
     //vars
     private int HoodMoveSpeed;
     private int[] PresetIndex = {0,0,0,0};
-    private double encoderToAngle;
+    private double angleToEncoder;
+    private double AbsoluteToMotor;
+    private double maxDegrees;
     private String State;
     
     //set inputs
@@ -58,17 +63,23 @@ public class HoodManager implements Subsystem{
         AutoAim.addInputListener(this);
         HoodMovement.addInputListener(this);
 
-        //motors also an encoder
-        HoodMotor = (WsSparkMax) Core.getOutputManager().getOutput(WSOutputs.HOOD_MOTOR);
-        
+        //vars
         HoodMoveSpeed = 10; //positive is up
-        encoderToAngle = 1; //number of encoder angle values per degrees
+        angleToEncoder = 1; //number of encoder angle values per degrees
+        AbsoluteToMotor = 10; // (Ea/MaxGa)
+        maxDegrees = 45;
+
+        //motors also an encoder
+        
+        HoodMotor = (WsSparkMax) Core.getOutputManager().getOutput(WSOutputs.HOOD_MOTOR);
+        HoodMotor.initClosedLoop(1.0,0.0,0.0,0.0);
+       
     }
 
     
     @Override
     public void update() {
-        if (State == "Manual"){ //generaly defult
+        if (State == "Manual"){ 
             if (HoodMovement.getValue() >0.75) {
                 HoodMotor.setSpeed(HoodMoveSpeed);
             }
@@ -76,45 +87,25 @@ public class HoodManager implements Subsystem{
                 HoodMotor.setSpeed(-HoodMoveSpeed);
             }
         }
-        if (State == "Auto"){
+        if (State == "Auto" && AutoAim.getValue() > 0.75){
             //intregrate jonahs stuff
             Double HoodAngle = (Double) Aim.getAngle();
-            if (HoodAngle > (Double) (HoodMotor.getValue() / encoderToAngle)){
-                HoodMotor.setSpeed(HoodMoveSpeed);
-            }else if (HoodAngle < (Double) (HoodMotor.getValue() / encoderToAngle)){
-                HoodMotor.setSpeed(-HoodMoveSpeed);
-            }
+            HoodMotor.setPosition(HoodAngle*angleToEncoder); //angle value to encoder
         }
         if (State == "P1"){
-            if (HoodMotor.getValue() > LC.Dists[PresetIndex[0]]){//hope that the presets are in encoder values
-                HoodMotor.setSpeed(HoodMoveSpeed);
-                
-            }else if (HoodMotor.getValue() < LC.Dists[PresetIndex[0]]){
-                HoodMotor.setSpeed(-HoodMoveSpeed);
-            }
+            HoodMotor.setPosition(PresetIndex[0]);
         }
         if (State == "P2"){
-            if (HoodMotor.getValue() > LC.Dists[PresetIndex[1]]){
-                HoodMotor.setSpeed(HoodMoveSpeed);
-                
-            }else if (HoodMotor.getValue() < LC.Dists[PresetIndex[1]]){
-                HoodMotor.setSpeed(-HoodMoveSpeed);
-            }
+            HoodMotor.setPosition(PresetIndex[1]);
         }
         if (State == "P3"){
-            if (HoodMotor.getValue() > LC.Dists[PresetIndex[2]]){
-                HoodMotor.setSpeed(HoodMoveSpeed);
-            }else if (HoodMotor.getValue() < LC.Dists[PresetIndex[2]]){
-                HoodMotor.setSpeed(-HoodMoveSpeed);
-            }
+            HoodMotor.setPosition(PresetIndex[2]);
         }
         if (State == "P4"){
-            if (HoodMotor.getValue() > LC.Dists[PresetIndex[3]]){
-                HoodMotor.setSpeed(HoodMoveSpeed);
-                
-            }else if (HoodMotor.getValue() < LC.Dists[PresetIndex[3]]){
-                HoodMotor.setSpeed(-HoodMoveSpeed);
-            }
+            HoodMotor.setPosition(PresetIndex[3]);
+        }
+        if (State == "Nutural"){ //requires buttons to be held down for it to move
+            
         }
     }
     
@@ -124,21 +115,24 @@ public class HoodManager implements Subsystem{
             State = "Auto";
         }
         //preset stuff
-        if (source == Preset1){
+        else if (source == Preset1){
             State = "P1";
         }
-        if (source == Preset2){
+        else if (source == Preset2){
             State = "P2";
         }
-        if (source == Preset3){
+        else if (source == Preset3){
             State = "P3";
         }
-        if (source == Preset4){
+        else if (source == Preset4){
             State = "P4";
         } 
         //manual controls
-        if(source == HoodMovement) {
+        else if((source == HoodMovement) && (HoodMovement.getValue() > 0.75 || HoodMovement.getValue() < -0.75) ){
             State = "Manual";
+        } 
+        else{
+            State = "Nutural";
         }
     }
 
@@ -150,7 +144,8 @@ public class HoodManager implements Subsystem{
     @Override
     public void resetState() {
         HoodMotor.setSpeed(0.0);
-        HoodMotor.setValue(0.0);//encoder to neo encoder calc (encoder - (Ea/MaxGa)*range)
+        HoodMotor.resetEncoder();
+        HoodMotor.setValue(trueEncoder.getValue() - (AbsoluteToMotor*maxDegrees));//encoder to neo encoder calc (encoder - (Ea/MaxGa)*range)
     }
 
     @Override
