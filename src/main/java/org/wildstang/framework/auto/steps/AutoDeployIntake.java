0dad783dc5415e9190804faf423dcb1f893dae63
StabStabImAChicken <77760254+StabStabImAChicken@users.noreply.github.com>
2022-02-09 19:32:00 -0600

    Autonomous Intake Update ver.1.0
    - created AutoDeployIntake.java step
    - added BALLPATH_INTAKE to enum in WSSubsystems.java
    - created AutoDeployIntake constrictor, initialize(), and update()
    - added if else statement in constrictor to deploy the intake and turn it on

---
+++ src/main/java/org/wildstang/framework/auto/steps/AutoDeployIntake.java
@@ -0,0 +1,42 @@
+package org.wildstang.framework.auto.steps;
+
+import org.wildstang.framework.auto.AutoStep;
+import org.wildstang.framework.core.Core;
+import org.wildstang.year2022.robot.WSSubsystems;
+import org.wildstang.year2022.subsystems.BallpathSubsystem_V2;
+
+public class AutoDeployIntake extends AutoStep {
+
+    private BallpathSubsystem_V2 intake;
+
+    public AutoDeployIntake(){
+        this(true);
+    }
+
+    public AutoDeployIntake(boolean deploy) {
+        intake = (BallpathSubsystem_V2) Core.getSubsystemManager().getSubsystem(WSSubsystems.BALLPATH_INTAKE.getName());
+        if (deploy){
+            intake.intakeDeploy();
+            intake.turnOnIntake();
+        }else{
+            intake.resetState();
+        }
+    }
+
+    @Override
+    public void initialize() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String toString() {
+        return "AutoDeployIntake";
+    }
+}
