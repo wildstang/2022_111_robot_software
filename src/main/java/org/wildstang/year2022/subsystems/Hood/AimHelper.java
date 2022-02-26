e86a8df6bcdedc8f5e1261a65326e92fc41bc8c3
bwaters2460 <bwaters2460@stu.d214.org>
2022-02-26 11:40:44 -0600

    brace

--- src/main/java/org/wildstang/year2022/subsystems/Hood/AimHelper.java
+++ src/main/java/org/wildstang/year2022/subsystems/Hood/AimHelper.java
@@ -35,7 +35,7 @@ import org.wildstang.year2022.subsystems.Hood.LimeConsts;
 
 import java.util.Arrays;
 
-public class AimHelper extends Subsystem{
+public class AimHelper extends Subsystem {
     
 	AnalogInput rightTrigger;
 	
@@ -89,7 +89,7 @@ public class AimHelper extends Subsystem{
 		
     }
 
-	}
+	
 	
 	@Override
 	public void selfTest() {
