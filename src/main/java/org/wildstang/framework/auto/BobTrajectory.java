5f9cd6d067000c92901d69ccc561809be2f75133
R3lity <55416813+jwaters3457@users.noreply.github.com>
2022-03-12 10:58:34 -0600

    Minor
    Added New Field Image

--- src/main/java/org/wildstang/framework/auto/BobTrajectory.java
+++ src/main/java/org/wildstang/framework/auto/BobTrajectory.java
@@ -3,7 +3,8 @@ package org.wildstang.framework.auto;
 import com.team319.ui.BobTrajectoryApp;
 
 public class BobTrajectory {
-    public static void main(String... args) {
+    
+    public static void main(String[] args) {
         new BobTrajectoryApp();
     }
 }
