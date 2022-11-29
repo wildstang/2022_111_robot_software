e4d175cf439085e6fb1beb531ee9a1f79a031278
potatomasqer <abrey3073@stu.d214.org>
2022-11-28 18:43:45 -0600

    fixed the 1 problem

--- src/main/java/org/wildstang/framework/auto/BobTrajectory.java
+++ src/main/java/org/wildstang/framework/auto/BobTrajectory.java
@@ -4,7 +4,7 @@ import com.team319.ui.BobTrajectoryApp;
 
 public class BobTrajectory {
     
-    public static void main(String[] args) {
+    public static void main(String... args) {
         new BobTrajectoryApp();
     }
 }
